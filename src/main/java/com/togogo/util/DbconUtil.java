e0f3caac7ca3da74dee1653370e1cdc9659bb98e
Haodadeyuang <57080327+Haodadeyuang@users.noreply.github.com>
2020-10-15 12:06:49 +0800

    新建数据库配置文件、数据库连接工具类 删除多余Dao层Account表操作 更新maven依赖

---
+++ src/main/java/com/togogo/util/DbconUtil.java
@@ -0,0 +1,45 @@
+package com.togogo.util;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+/**
+ * @description:数据库连接工具类
+ * @author: Haodadeyu
+ * @time: 2020/10/15 11:25
+ */
+public class DbconUtil {
+
+        private static Connection con;
+        private static String driverClass;
+        private static String url;
+        private static String user;
+        private static String password;
+
+        static{
+            try {
+                readConfig();
+                Class.forName(driverClass);
+                con = DriverManager.getConnection(url, user, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private static void readConfig() throws Exception{
+            InputStream in = DbconUtil.class.getClassLoader().getResourceAsStream("database.properties");
+            Properties pro = new Properties();
+            pro.load(in);
+            driverClass = pro.getProperty("JDBC_DRIVERCLASS");
+            url = pro.getProperty("JDBC_URL");
+            user = pro.getProperty("JDBC_USER");
+            password = pro.getProperty("JDBC_PASSWORD");
+
+        }
+        public static Connection getConnection(){
+            return con;
+        }
+
+}
